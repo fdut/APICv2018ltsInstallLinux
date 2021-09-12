a64f7dae6e84feba558e709a92a01af1467e7f66
fdut <fdutheil@laposte.net>
2021-09-12 13:48:16 +0200

    Add docker-registry

---
+++ bin/install-registry.sh
@@ -0,0 +1,6 @@
+helm repo add docker-registry https://charts.helm.sh/stable/
+helm repo update
+helm install registry stable/docker-registry  -f yaml/docker-registry-values.yaml
+echo "Waiting for docker registry to start"
+bash bin/sleep.sh 60
+kubectl get po -A
